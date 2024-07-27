2e55e05a725ab213448d829d2d8c41bef47fd655
Prince <prince02@gmail.com>
2024-07-27 17:17:18 +0530

    first commit

---
+++ src/test/java/com/prince/testsample/CalculatorTest.java
@@ -0,0 +1,49 @@
+package com.prince.testsample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+	
+	private Calculator calculator;
+	
+	@BeforeEach
+	public void setUp() {
+		calculator = new Calculator();
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		calculator = null;
+		System.out.println("Test Finished");
+	}
+	
+	
+	
+	@Test
+	void testAdd() {
+		int result = calculator.add(2, 3);
+		assertEquals(5,result);
+		
+	}
+
+	@Test
+	void testSubtract() {
+		int result = calculator.subtract(5, 3);
+		assertEquals(2,result);
+			}
+	
+
+//	@Disabled("Disabled until bug #123 is fixed")
+	@Test
+	public void testSubtractNegativeResult() {
+		int result = calculator.subtract(3,5);
+		assertEquals(-2,result);
+	}
+	
+
+}
